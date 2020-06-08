213359f248f82ee84307f7993d69667d9ca05479
Jayne Doe <jayne.doe@ibm.com>
2020-06-08 23:47:15 +0100

    feat(phrases): crowbar in simple phrases to the flashcards

--- src/ts/objects/flashcard.ts
+++ src/ts/objects/flashcard.ts
@@ -1,8 +1,10 @@
 import {DictionaryFlashcard} from "./dictionary-flashcard";
 import {KanaFlashcard} from "./kana-flashcard";
 import {KanjiFlashcard} from "./kanji-flashcard";
+import {PhraseFlashcard} from "./phrase-flashcard";
 
 export type FullKanaFlashcard = DictionaryFlashcard & KanaFlashcard;
 export type FullKanjiFlashcard = DictionaryFlashcard & KanjiFlashcard;
+export type FullPhraseFlashcard = DictionaryFlashcard & PhraseFlashcard;
 
-export type Flashcard = FullKanaFlashcard | FullKanjiFlashcard;
+export type Flashcard = FullKanaFlashcard | FullKanjiFlashcard | PhraseFlashcard;
